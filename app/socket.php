3570a837a8890442ca9f9e20fcc5a01d7fce4cff
Zane Oliver <zaneoliver6@gmail.com>
2021-01-26 18:16:11 -0600

    changes

--- app/socket.php
+++ app/socket.php
@@ -7,36 +7,36 @@ use Ratchet\ConnectionInterface;
 
 class Socket implements MessageComponentInterface {
 
-    public function __construct()
-    {
-        $this->clients = new \SplObjectStorage;
-    }
+   public function __construct()
+   {
+      $this->clients = new \SplObjectStorage;
+   }
 
-    public function onOpen(ConnectionInterface $conn) {
+   public function onOpen(ConnectionInterface $conn) {
 
         // Store the new connection in $this->clients
-        $this->clients->attach($conn);
+      $this->clients->attach($conn);
 
-        echo "New connection! ({$conn->resourceId})\n";
-    }
+      echo "New connection! ({$conn->resourceId})\n";
+   }
 
-    public function onMessage(ConnectionInterface $from, $msg) {
+   public function onMessage(ConnectionInterface $from, $msg) {
 
-        foreach ( $this->clients as $client ) {
+      foreach ( $this->clients as $client ) {
 
             if ( $from->resourceId == $client->resourceId ) {
-                continue;
+               continue;
             }
 
             $client->send( "Client $from->resourceId said $msg" );
-        }
-    }
+      }
+   }
 
-    public function onClose(ConnectionInterface $conn) {
-    }
+   public function onClose(ConnectionInterface $conn) {
+   }
 
-    public function onError(ConnectionInterface $conn, \Exception $e) {
-    }
+   public function onError(ConnectionInterface $conn, \Exception $e) {
+   }
 }
 
 ?>
